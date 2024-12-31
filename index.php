8c8110a71bf4ceaf1446cb98d420a0712aee4d6a
mbsoft31 <mbsoft31@users.noreply.github.com>
2024-12-31 19:39:28 +0000

    Fix styling

--- index.php
+++ index.php
@@ -5,16 +5,23 @@ require 'vendor/autoload.php';
 class AlgerianNationalID
 {
     protected string $id;
+
     protected string $sex = '';
+
     protected int $yearOfBirth = 0;
+
     protected string $cityOfBirth = '';
+
     protected string $certificateID = '';
+
     protected string $registryNumber = '';
+
     protected bool $processed = false;
 
     protected bool $isValid = false;
 
-    public function __construct(string $id) {
+    public function __construct(string $id)
+    {
         $this->id = $id;
     }
 
@@ -41,11 +48,11 @@ class AlgerianNationalID
         $registryID = substr($this->id, 14, 4);
 
         // Validate and set properties
-        if (!$this->validateSexCode($sexCode) ||
-            !$this->validateYearOfBirth($yearOfBirth) ||
-            !$this->validateNumericFormat($communeOfBirth, 4) ||
-            !$this->validateNumericFormat($civilStateRegistryID, 5) ||
-            !$this->validateNumericFormat($registryID, 4)) {
+        if (! $this->validateSexCode($sexCode) ||
+            ! $this->validateYearOfBirth($yearOfBirth) ||
+            ! $this->validateNumericFormat($communeOfBirth, 4) ||
+            ! $this->validateNumericFormat($civilStateRegistryID, 5) ||
+            ! $this->validateNumericFormat($registryID, 4)) {
             return false;
         }
 
@@ -54,6 +61,7 @@ class AlgerianNationalID
         $this->registryNumber = $registryID;
 
         $this->isValid = true;
+
         return true;
     }
 
@@ -66,17 +74,18 @@ class AlgerianNationalID
         } else {
             return false;
         }
+
         return true;
     }
 
     protected function validateYearOfBirth(string $yearOfBirth): bool
     {
-        if (!preg_match('/^\d{3}$/', $yearOfBirth)) {
+        if (! preg_match('/^\d{3}$/', $yearOfBirth)) {
             return false;
         }
 
         $century = $yearOfBirth[0] === '9' ? '19' : '20';
-        $this->yearOfBirth = (int)($century . substr($yearOfBirth, 1));
+        $this->yearOfBirth = (int) ($century.substr($yearOfBirth, 1));
 
         if ($century !== '20') {
             return true;
@@ -107,19 +116,20 @@ class AlgerianNationalID
     public function print(): string
     {
         if ($this->isValid) {
-            return "The ID is valid.\n" .
-                (($this->sex === 'male') ? 'Sex: Male/10' : 'Sex: Female/11') . "\n" .
-                "Year of Birth: {$this->yearOfBirth}\n" .
-                "City of Birth: {$this->cityOfBirth}\n" .
-                "Certificate ID: {$this->certificateID}\n" .
+            return "The ID is valid.\n".
+                (($this->sex === 'male') ? 'Sex: Male/10' : 'Sex: Female/11')."\n".
+                "Year of Birth: {$this->yearOfBirth}\n".
+                "City of Birth: {$this->cityOfBirth}\n".
+                "Certificate ID: {$this->certificateID}\n".
                 "Registry Number: {$this->registryNumber}\n";
         }
+
         return "The ID is invalid.\n";
     }
 }
 
 // Example usage
-$id = "109941329027780000"; // Replace with actual ID for testing
+$id = '109941329027780000'; // Replace with actual ID for testing
 $algerianID = new AlgerianNationalID($id);
 $algerianID->process();
 
--- src/Rewards.php
+++ src/Rewards.php
@@ -2,7 +2,4 @@
 
 namespace Mbsoft\Rewards;
 
-class Rewards
-{
-
-}
+class Rewards {}
